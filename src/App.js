dde6a165fcd90823360bcc932a370b747d58a4bc
ZumeShougo <g0109725rena@gmail.com>
2023-02-14 16:10:53 +0900

    Add signIn&signOut

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Line from "./components/Line";
+import SignIn from './components/SignIn';
+import { useAuthState } from "react-firebase-hooks/auth";//firebaseのhooks
+import { auth } from "./firebase.js";
 
 function App() {
+  const [user] = useAuthState(auth);//userの認証状態 userは角括弧で括らなければならない
   return (
-    <div>
-
-    </div>
+    <div className="App">
+      {user ? <Line /> : <SignIn />}
+    </div>//userが存在すれば<Line />存在しなければ<SignIn />
   );
 }
 
---
+++ src/components/Line.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignOut from './SignOut.js'
+
+function line() {
+  return (
+    <div>
+      <SignOut />
+    </div>
+  )
+}
+
+export default line
