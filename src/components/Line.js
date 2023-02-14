3fc1d8f32acf01fea84181ec354e9c370f0780ab
ZumeShougo <g0109725rena@gmail.com>
2023-02-14 16:58:27 +0900

    PickUp data

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import SignOut from './SignOut.js'
+import React, { useEffect, useState } from 'react';
+import SignOut from './SignOut.js';
+import SendMessage from './SendMessage.js'
+import { db } from '../firebase.js';
 
-function line() {
+function Line() {
+  const [messages, setMessages] = useState([]);//配列にしておかないとmap関数が使えない
+  useEffect(() => {
+    db.collection("messages")//DBにあるmessages
+    .orderBy("createdAt")//登録された順に取ってくる
+    .limit(50)//50個まで取ってくる
+    .onSnapshot((snapshot) => {
+      setMessages(snapshot.docs.map((doc) => doc.data()));//snapshot・・・「状態を抜き取ったもの」
+    })
+  }, []);
   return (
     <div>
+      {console.log(messages)}
       <SignOut />
+      <div className='megs'>
+        {messages.map(({ id, text, photoURL, uid }) => (
+          <div key={id}>
+            <img src={photoURL} alt="" />
+            <p>{text}</p>
+          </div>
+        ))}
+      </div>
+      <SendMessage />
     </div>
   )
 }
 
-export default line
+export default Line
